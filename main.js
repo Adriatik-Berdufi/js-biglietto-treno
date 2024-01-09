8b75b74fd44382379b8eb6708d38e49399f2f4a7
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-01-09 17:33:38 +0100

    feat: add error case

--- main.js
+++ main.js
@@ -7,14 +7,23 @@ console.log(prezzoPerKm);
 let prezzo;
 let prezzoFinale = document.getElementById("prezzofinale") ;
 
-if (userEta < 18){
-    prezzo = (kmDaPerCorrere * 0.8 * 0.21).toFixed(2);
-    console.log(prezzo);
-}else if (userEta >= 65){
-    prezzo = (kmDaPerCorrere * 0.6 * 0.21).toFixed(2);
-    console.log(prezzo);
+if(isNaN(userEta) || isNaN(kmDaPerCorrere)){
+    error = true;
+    prezzoFinale.innerText = "i dati insseriti non son correti. RIPROVA " ;
 }else{
-    prezzo = (kmDaPerCorrere * 0.21).toFixed(2);
-    console.log(prezzo);
+    error = false;
 }
-prezzoFinale.innerText = "il prezzo del biglietto e: " +  prezzo + "$";
+
+if(!error){
+    if (userEta < 18){
+        prezzo = (kmDaPerCorrere * 0.8 * 0.21).toFixed(2);
+        console.log(prezzo);
+    }else if (userEta >= 65){
+        prezzo = (kmDaPerCorrere * 0.6 * 0.21).toFixed(2);
+        console.log(prezzo);
+    }else{
+        prezzo = (kmDaPerCorrere * 0.21).toFixed(2);
+        console.log(prezzo);
+    }
+    prezzoFinale.innerText = "il prezzo del biglietto e: " +  prezzo + "$";
+}
